0158acdb24e1e354678b8705eba6de7a97dcbb9a
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2022-12-23 12:01:49 +0200

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by KhaleD HuSsien on 23/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
